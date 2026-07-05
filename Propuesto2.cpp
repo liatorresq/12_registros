301e5097243ed070e41fb7926c4d5dde03b1b25d
liatorresq <ltorresq@unjbg.edu.pe>
2026-07-05 16:12:31 -0500

    Propuesto2: primer intento

---
+++ Propuesto2.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Persona {
+    string nombres;
+    string dni;
+    int edad;
+};
+
+int main() {
+    int n;
+    cout << "Ingrese la cantidad de personas a registrar: ";
+    cin >> n;
+
+    Persona lista[100];
+
+    for (int i = 0; i < n; i++) {
+        cout << "\n--- Persona " << i + 1 << " ---" << endl;
+        cout << "Ingrese nombre (sin espacios): ";
+        cin >> lista[i].nombres;
+        cout << "Ingrese DNI: ";
+        cin >> lista[i].dni;
+        cout << "Ingrese edad: ";
+        cin >> lista[i].edad;
+    }
+
+    int mayores50 = 0;
+    int sumaEdades = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (lista[i].edad > 50) {
+            mayores50++
+        }
+        sumaEdades = sumaEdades + lista[i].edad;
+    }
+
+    int promedio = sumaEdades / n;
+
+    cout << "\n================ RESULTADOS ================" << endl;
+    cout << "Cantidad de personas mayores de 50 anos: " << mayores50 << endl;
+    cout << "El promedio de edades es: " << promedio << endl;
+
+    cout << "\n========= LISTADO DE PERSONAS =========" << endl;
+    for (int i = 0; i < n; i++) {
+        cout << "Persona " << i + 1 << ": " << lista[i].nombres;
+        cout << " - DNI: " << lista[i].dni;
+        cout << " - Edad: " << lista[i].edades << endl;
+    }
+
+    return 0;
+}
